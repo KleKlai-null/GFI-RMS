b1d9f8c41d56aa95d3c9d296867d980592acdbce
TheCodingCat <mmagallen_180000002192@uic.edu.ph>
2022-09-14 03:43:27 +0800

    added 500 error page

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,7 +1,9 @@
 <x-tabler-guest-layout :code="500">
     <div class="container-tight py-4">
         <div class="empty">
-            <div class="empty-header">500</div>
+            <div class="empty-header">
+                <img src="{{ asset('asset/images/errors/500.svg') }}" height="300"
+                    alt=""></div>
             <p class="empty-title">Internal Server Error</p>
             <p class="empty-subtitle text-muted">
                 The server has been deserted for a while. Please be patient or try again later.
