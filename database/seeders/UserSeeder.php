4f14b2744738d417becd11030e62bf71dc06a55f
TheCodingCat <mmagallen_180000002192@uic.edu.ph>
2022-10-28 16:52:54 +0800

    Included seeder of epoy account

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -111,6 +111,16 @@ class UserSeeder extends Seeder
             'remember_token' => Str::random(10),
         ]);
 
+        $epoy = User::create([
+            'first_name' => 'John Paler',
+            'last_name' => 'Rodolph',
+            'username' => 'jrodolph',
+            'email' => 'rqpaler@gensanfeedmill.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+
         $admin->syncRoles('administrator');
     }
 }
